fab10c9a93c8c30d64258c815cdfee877aa46c61
Madeline0421 <madelinehou421@gmail.com>
2023-01-05 03:03:58 -0500

    Add ability to calculate with typed inputs

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ class Calculator {
         this.previousOperand = '';
         this.currentOperand = '';
         this.operator = '';
+        display.textContent = '';
     }
 
     add(num1, num2) {
@@ -36,23 +37,29 @@ class Calculator {
             case '+':
                 total = this.add(this.previousOperand, this.currentOperand);
                 break;
-            case '-':
+            case '−':
                 total = this.subtract(this.previousOperand, this.currentOperand);
                 break;
-            case '*':
+            case '×':
                 total = this.multiply(this.previousOperand, this.currentOperand);
                 break;
-            case '/':
+            case '÷':
                 total = this.divide(this.previousOperand, this.currentOperand);
                 break;
-        }
+        }        
+        console.log(this.previousOperand);
+        console.log(this.currentOperand);
+        console.log(this.operator);
+        
         this.previousOperand = total; 
         this.currentOperand = '';
         this.operator = '';
+
+        addDisplay("=" + total);
         return total;
     }
 
-    chooseOperator(operator) { 
+    appendOperator(operator) { 
         if (this.previousOperand == '') { 
             this.previousOperand = this.currentOperand;
             this.operator = operator;
@@ -60,13 +67,65 @@ class Calculator {
         } else { 
             this.operator = operator;
         }
+        addDisplay(operator);
     }
 
     appendNumber(number) { 
         this.currentOperand += number.toString(); 
+        addDisplay(number);
+    }
+
+    isValid() {
+        return this.currentOperand != '' && this.previousOperand != '' && this.operator != '';
+    }
+
+    convertOperator(operator) {
+        switch (operator) { 
+            case "*" :
+                return '×';
+            case '/':
+                return '÷';
+            case '-':
+                return '−';
+            case '+': 
+                return '+';
+        }
+    }
+
+    deleteNumber() {
+        this.currentOperand = '';
     }
 
 }
+
+// const aliasKeys = {
+//     "ctrl" : 17,
+//     "shift" : 16,
+//     "multiply" : 106,
+//     "add" : 107,
+//     "subtract" : 109,
+//     "decimal-point" : 110,
+//     "divide" : 111,
+//     "equal-sign" : 187,
+// }
+const aliasKeys = [];
+for (let i = 0; i <= 9; i++) {
+    aliasKeys[i] = i;
+}
+
+// function checkKey(key) { 
+//     return key in keys;
+// }
+
+// function checkKeys() {
+//     let keylist = arguments;
+//     console.log("check");
+//     for (let i = 0; i < keylist.length; i++) {
+//         console.log(keylist[i]);
+//     }
+//     return true;
+// }
+
 
 let numbers = document.querySelectorAll('.number');
 let display = document.querySelector('#display');
@@ -74,10 +133,37 @@ let operators = document.querySelectorAll('.operator');
 let equalSign = document.querySelector('#equal-sign');
 let clear = document.querySelector('#clear');
 
+document.addEventListener('keydown', addCharacters, false); 
+
+
+function addCharacters(e) {
+    if (e.key >= 0 && e.key <= 9) { 
+        calculator.appendNumber(e.key);
+    } else if (e.key == '*' || e.key == '/' || e.key == '+' || e.key == '-') {
+        calculator.appendOperator(calculator.convertOperator(e.key));
+    } else if (e.key == '=' || e.key == 'Enter') {
+        calculator.calculate();
+    } else if (e.key == 'Backspace') {
+        calculator.deleteNumber();
+        removeDisplay();
+    } else if (e.key == "C" || e.key == "c") { 
+        calculator.clear(); 
+    }
+    
+}
+
+function addDisplay(value) {
+    display.textContent += value;
+}
+
+function removeDisplay(value) {
+    display.textContent = display.textContent.toString().slice(0, -1);
+}
+
 let calculator = new Calculator('', '', null);
 operators.forEach((operator) => {
     operator.addEventListener('click', () => {
-        calculator.chooseOperator(operator.innerHTML);
+        calculator.appendOperator(calculator.convertOperator(operator.innerHTML));
     });
 });
 
@@ -88,13 +174,8 @@ numbers.forEach((number) => {
 });
 
 equalSign.addEventListener('click', () => {
-    console.log("prev:" + calculator.previousOperand);
-    console.log("current: " + calculator.currentOperand);
-    console.log("operator: " + calculator.operator);
-    console.log(calculator.calculate());
-    console.log("prev:" + calculator.previousOperand);
-    console.log("current: " + calculator.currentOperand);
-    console.log("operator: " + calculator.operator);
+    calculator.calculate();
+
 });
 
 clear.addEventListener('click', () => { 
